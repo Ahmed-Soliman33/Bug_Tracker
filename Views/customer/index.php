180296ed80937666864aec0d377883a2dbfd56d5
Ahmed Salah <saa774043@gmail.com>
2025-05-06 15:28:23 +0300

    update

--- Views/customer/index.php
+++ Views/customer/index.php
@@ -22,7 +22,7 @@ if (!isset($_SESSION["userRole"])) {
   <meta name="viewport"
     content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-  <title>Dashboard</title>
+  <title>Customer</title>
 
   <meta name="description" content="" />
 
