286a315c2dceeebc09cef785294490f321e2ab5a
Ahmed Salah <saa774043@gmail.com>
2025-05-06 15:29:21 +0300

    customer edit

--- Views/customer/index.php
+++ Views/customer/index.php
@@ -22,7 +22,7 @@ if (!isset($_SESSION["userRole"])) {
   <meta name="viewport"
     content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-  <title>Customer</title>
+  <title>Customer Page</title>
 
   <meta name="description" content="" />
 
